eb72d809ead23476e65d24a662567f0999858cae
Wasef07 <skwasefmostafa3@gmail.com>
2026-09-07 11:29:49 -0400

    Fix remaining Python lint errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'skwasefmosta-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    ('skwasefmosta-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
+     'proxy.cognitiveclass.ai')
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://skwasefmosta-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    ('https://skwasefmosta-8000.theianext-1-labs-prod-misc-tools-us-east-0.'
+     'proxy.cognitiveclass.ai')
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -97,7 +99,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        ('django.contrib.auth.password_validation.'
+         'UserAttributeSimilarityValidator'),
     },
     {
         'NAME':
